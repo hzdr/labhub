52449adbbe30b4183d549c2d8c32a3a54c86df78
Christian Hueser <55481699+christianhueserhzdr@users.noreply.github.com>
2020-01-06 11:49:50 +0100

    Try GitHub pull request trigger actions (#2)
    * Add more pull request trigger actions
    
    Added "opened", "reopened", "closed" (merged) as pull request trigger actions.
    
    * Remove some unneccessary pull request trigger actions
    
    Not all trigger actions are relevant. Removed some but "synchronized",
    "opened", "reopend".
    
    * Simplify check for pull request trigger actions
    
    Check action "merged" instead of action "closed".
    
    * Re-enable "closed" trigger action
    
    Check if key "merged" is true if action is "closed".
    
    * Replaced "merged" by "closed" pull request trigger action
    
    There is no pull request trigger action called "merged". It is included
    in "closed".
    
    * Add all possible options of github trigger actions
    
    Configuration of enabled trigger actions need to be done in LabHub TOML
    configuration file.

--- src/actions/actions.rs
+++ src/actions/actions.rs
@@ -2,8 +2,20 @@
 #[derive(Debug, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub enum ActionValue {
+    Assigned,
+    Closed,
+    Edited,
+    Labeled,
+    Locked,
+    Opened,
+    ReadyForReview,
+    Reopened,
     ReviewRequested,
+    ReviewRequestRemoved,
     Synchronize,
+    Unassigned,
+    Unlabeled,
+    Unlocked,
 }
 
 #[derive(Debug, PartialEq)]
